b755426bca37dc7fcfa0dc588cf5c261a0d74503
Jenna Chestnut <jennabot5000@gmail.com>
2021-11-14 17:43:27 -0600

    removed "is provider" validation from restricted access file

--- src/middleware/restricted-access.js
+++ src/middleware/restricted-access.js
@@ -3,8 +3,8 @@
 function checkRestrictedAccess(req, res, next) {
   const { user } = req;
 
-  // if user is not an admin or provider, 401
-  if (!user.is_admin && !user.is_provider) {
+  // if user is not an admin, 401
+  if (!user.is_admin) {
     return res.status(401).json({
       error: { 
         message: 'Unauthorized request'
